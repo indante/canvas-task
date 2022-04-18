7fa679ca82407959065e3e95256374d082b7c2db
indante <koreanhoseon@gmail.com>
2022-04-19 03:38:08 +0900

    Update getMovement function
    각도에 따라 x, y 이동값 계산해주는 함수 구현

--- canvas.js
+++ canvas.js
@@ -1,43 +1,54 @@
 const canvas = document.getElementById("myCanvas");
 
 class Ball {
+  speed = 2;
   ctx;
-  horizontal;
-  vertical;
+  x;
+  y;
   radius;
-  moveHorizontal;
-  moveVertical;
+  degree;
 
-  constructor(ctx, horizontal, vertical, radius, moveHorizontal, moveVertical) {
+  constructor(ctx, x, y, radius, degree) {
     this.ctx = ctx;
-    this.horizontal = horizontal;
-    this.vertical = vertical;
+    this.x = x;
+    this.y = y;
     this.radius = radius;
-    this.moveHorizontal = moveHorizontal;
-    this.moveVertical = moveVertical;
-  };
+    this.degree = degree;
+  }
 
   draw(fillStyle) {
     this.ctx.beginPath();
-    this.ctx.fillStyle = fillStyle
-    this.ctx.arc(this.horizontal, this.vertical, this.radius, 0, Math.PI * 2);
+    this.ctx.fillStyle = fillStyle;
+    this.ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
     this.ctx.fill();
     this.ctx.closePath();
-  };
+  }
 
   moveBall() {
-    if (this.vertical + this.moveVertical > canvas.height - this.radius || this.vertical + this.moveVertical < this.radius) {
-      this.moveVertical = -this.moveVertical;
+    let { x, y } = getMovement(this.degree);
+    
+    let xMovement = x * this.speed;
+    let yMovement = y * this.speed;
+
+
+    if (
+      this.x + xMovement > canvas.width - this.radius ||
+      this.x + xMovement < this.radius
+    ) {
+      xMovement = -xMovement;
     }
 
-    if (this.horizontal + this.moveHorizontal > canvas.width - this.radius || this.horizontal + this.moveHorizontal < this.radius) {
-      this.moveHorizontal = -this.moveHorizontal;
+    if (
+      this.y + yMovement > canvas.height - this.radius ||
+      this.y + yMovement < this.radius
+    ) {
+      yMovement = -yMovement;
     }
 
-    this.horizontal += this.moveHorizontal;
-    this.vertical += this.moveVertical;
-  };
-};
+    this.x += xMovement;
+    this.y += yMovement;
+  }
+}
 
 function init() {
   const canvas = document.getElementById("myCanvas");
@@ -45,52 +56,84 @@ function init() {
   const randomNumberOfBalls = getRandomArbitrary(10, 20);
 
   const balls = [];
-  
-  for (let i = 0; i < 5; i++) {
-    const randomBallRaidus = getRandomArbitrary(10, 20);
-    const horizontal = getRandomArbitrary(randomBallRaidus, canvas.width - randomBallRaidus);
-    const vertical = getRandomArbitrary(randomBallRaidus, canvas.height - randomBallRaidus);
 
-    const ball = new Ball(
-      ctx,
-      horizontal,
-      vertical,
+  for (let i = 0; i < 1; i++) {
+    const randomBallRaidus = getRandomArbitrary(10, 20);
+    const randomDegree = getRandomArbitrary(0, 360);
+    const randomX = getRandomArbitrary(
       randomBallRaidus,
-      getRandomDistanceToMove(),
-      getRandomDistanceToMove(),
+      canvas.width - randomBallRaidus
     );
+    const randomY = getRandomArbitrary(
+      randomBallRaidus,
+      canvas.height - randomBallRaidus
+    );
+
+    const ball = new Ball(ctx, randomX, randomY, randomBallRaidus, randomDegree);
     balls.push(ball);
-  };
+  }
 
   setInterval(() => {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     balls.forEach((ball, i) => {
-      let color = 'black';
+      let color = "black";
       for (let j = 0; j < balls.length; j++) {
         if (i == j) {
           continue;
         }
-        const d = Math.sqrt(Math.pow(balls[i].horizontal - balls[j].horizontal, 2) + Math.pow(balls[i].vertical - balls[j].vertical, 2))
+        const d = Math.sqrt(
+          Math.pow(balls[i].x - balls[j].x, 2) +
+            Math.pow(balls[i].y - balls[j].y, 2)
+        );
 
         if (balls[i].radius + balls[j].radius > d) {
-          color = 'red'
+          color = "red";
           break;
-        } 
+        }
       }
       ball.draw(color);
       ball.moveBall();
     });
   }, 1000 / 60);
-};
+}
 
 function getRandomArbitrary(min, max) {
   return Math.floor(Math.random() * (max - min + 1) + min);
-};
+}
 
-function getRandomDistanceToMove() {
-  return getRandomArbitrary(1, 10) % 2 === 0 ? getRandomArbitrary(1, 4) : getRandomArbitrary(-4, -1);
-};
+// function getRandomDistanceToMove() {
+//   return getRandomArbitrary(1, 10) % 2 === 0
+//     ? getRandomArbitrary(1, 4)
+//     : getRandomArbitrary(-4, -1);
+// }
 
+function getMovement(degree) {
+  let x;
+  let y;
 
+  if (degree < 90) {
+    x = degree / 90;
+    y = (90 - degree) / 90;
+  }
+
+  if (degree >= 90 && degree < 180) {
+    x = (180 - degree) / 90;
+    y = (180 - degree - 90) / 90;
+  }
+
+  if (degree >= 180 && degree < 270) {
+    x = -(degree % 90) / 90;
+    y = -(90 - (degree % 90)) / 90;
+  }
+
+  if (degree >= 270 && degree <= 360) {
+    x = -(360 - degree) / 90;
+    y = -(360 - degree - 90) / 90;
+  }
+
+  console.log({x, y})
+
+  return { x, y };
+}
 
 init();
